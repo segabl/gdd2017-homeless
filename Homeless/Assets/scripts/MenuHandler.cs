f1ee88b74b15b50b60e579a0ef4c5a5059f301f6
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-12-22 12:28:30 +0100

    Create main menu

---
+++ Homeless/Assets/scripts/MenuHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour {
+
+  public Button b_start;
+  public Button b_continue;
+  public Button b_end;
+
+	// Use this for initialization
+	void Start () {
+    if (System.IO.File.Exists("savefile.txt")) {
+      b_continue.interactable = true;
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+  public void gameStart() {
+    Debug.Log("Start game");
+  }
+
+  public void gameContinue() {
+    Debug.Log("Continue game");
+  }
+
+  public void gameEnd() {
+    Debug.Log("End game");
+    #if UNITY_EDITOR
+      UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+    Application.Quit();
+  }
+}
