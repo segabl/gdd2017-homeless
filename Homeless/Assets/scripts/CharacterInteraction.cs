ab6c19e716c0dcb9ab2fdd650c856a8445188f5b
Christian Huber <chuber@student.tugraz.at>
2018-01-04 17:03:21 +0100

    Interact with other objects

---
+++ Homeless/Assets/scripts/CharacterInteraction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInteraction : InteractionHandler {
+
+  public override void interact() {
+    text.text = "This is a character text";
+  }
+
+}
---
+++ Homeless/Assets/scripts/InteractionHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InteractionHandler : MonoBehaviour {
+
+  public GameObject mainCharacter;
+  public GameObject canvas;
+  public float triggerDistance;
+  public UnityEngine.UI.Text text;
+  private string defaultText;
+
+  void Start() {
+    defaultText = text.text;
+  }
+
+	void Update () {
+    if (Vector3.Distance(this.transform.position, mainCharacter.transform.position) < triggerDistance) {
+      canvas.SetActive(true);
+      if (Input.GetKeyDown("e")) {
+        interact();
+      }
+    }
+    else {
+      canvas.SetActive(false);
+      text.text = defaultText;
+    }
+  }
+
+  public abstract void interact();
+
+}
