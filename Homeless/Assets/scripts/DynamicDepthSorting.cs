4da0db3d7f6d0a6eba2eaa8da941f1cf2171ea97
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-01-04 19:59:27 +0100

    Add dynamic depth sorting based on y value

---
+++ Homeless/Assets/scripts/DynamicDepthSorting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicDepthSorting : MonoBehaviour {
+
+  private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+    spriteRenderer = this.GetComponent<SpriteRenderer>();
+    spriteRenderer.sortingLayerName = "Dynamic";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    spriteRenderer.sortingOrder = (int) this.transform.position.y * (-10);
+	}
+}
