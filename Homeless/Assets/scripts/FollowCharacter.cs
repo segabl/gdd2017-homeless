b71fd81ac65c8b6307588825667a42c2fb77968d
Christian Huber <chuber@student.tugraz.at>
2018-01-04 12:37:20 +0100

    Camera follows player

---
+++ Homeless/Assets/scripts/FollowCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCharacter : MonoBehaviour {
+
+  private Vector3 targetPosition;
+  public float movementSpeed;
+
+  void Start () {
+    targetPosition = this.transform.position;
+	}
+	
+	void Update () {
+    GameObject mainCharacter = GameObject.Find("MainCharacter");
+    targetPosition = mainCharacter.transform.position;
+    targetPosition.z = this.transform.position.z;
+    if (this.transform.position != targetPosition) {
+      float step = movementSpeed * Time.deltaTime;
+      this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, step);
+    }
+  }
+}
