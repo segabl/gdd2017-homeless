e82249fc32dc082c88e1f8e24928faef4cfb6e37
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-01-09 20:42:40 +0100

    Refactor GameController

--- Homeless/Assets/scripts/GameController.cs
+++ Homeless/Assets/scripts/GameController.cs
@@ -3,28 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class GameController {
-  
-  public GameObject player { get; set; }
-  public GameObject screenCanvas { get; set; }
-  public GameObject menuCanvas { get; set; }
+public class GameController : PausableObject {
 
-  public float dayLength { get; set; }
-  private float accumulatedDelta = 0;
-  private int day = 0;
-  public float dayTime { get; set; }
+  public GameObject player;
+  public GameObject screenCanvas;
+  public GameObject menuCanvas;
+  public float dayLength;
 
+  private float accumulatedDelta;
+  public int day { get; private set; }
+  public float dayTime { get; private set; }
+
+  private static GameObject controllerObject;
   private static GameController controllerInstance;
+
   public static GameController instance {
     get {
-      if (controllerInstance == null) {
-        controllerInstance = new GameController();
+      if (!controllerObject) {
+        GameObject obj = new GameObject("GameController");
+        return obj.AddComponent<GameController>();
       }
       return controllerInstance;
     }
   }
 
-	public void update() {
+  void Awake() {
+    DontDestroyOnLoad(gameObject);
+    if (controllerInstance) {
+      Debug.Log("Controller exists, only taking new editor params");
+      controllerInstance.player = player;
+      controllerInstance.dayLength = dayLength;
+      controllerInstance.screenCanvas = screenCanvas;
+      controllerInstance.menuCanvas = menuCanvas;
+      Destroy(gameObject);
+    } else {
+      Debug.Log("Controller created");
+      controllerObject = gameObject;
+      controllerInstance = this;
+    }
+    day = 0;
+    accumulatedDelta = 0;
+  }
+
+  protected override void updatePausable() {
+    if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name != "GameScene") {
+      return;
+    }
     accumulatedDelta += Time.deltaTime;
     dayTime = accumulatedDelta / dayLength;
     if (dayTime > 1) {
