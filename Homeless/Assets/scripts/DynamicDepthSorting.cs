c02f62e2db8378169c368626baa72b18ab6cddd4
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-01-09 11:57:57 +0100

    Extend sorting to work with Spriter prefabs

--- Homeless/Assets/scripts/DynamicDepthSorting.cs
+++ Homeless/Assets/scripts/DynamicDepthSorting.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SpriterDotNetUnity;
 
 public class DynamicDepthSorting : MonoBehaviour {
 
   private SpriteRenderer spriteRenderer;
+  private SpriterDotNetBehaviour spriterBehaviour;
 
-	// Use this for initialization
-	void Start () {
-    spriteRenderer = this.GetComponent<SpriteRenderer>();
-    spriteRenderer.sortingLayerName = "Dynamic";
+  // Use this for initialization
+  void Start () {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    if (spriteRenderer) {
+      spriteRenderer.sortingLayerName = "Dynamic";
+    }
+    spriterBehaviour = GetComponent<SpriterDotNetBehaviour>();
+    if (spriterBehaviour) {
+      spriterBehaviour.SortingLayer = "Dynamic";
+    }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    spriteRenderer.sortingOrder = (int) this.transform.position.y * (-10);
-	}
+    if (spriteRenderer) {
+      spriteRenderer.sortingOrder = (int)(transform.position.y * (-50));
+    }
+    if (spriterBehaviour) {
+      spriterBehaviour.SortingOrder = (int)(transform.position.y * (-50));
+    }
+  }
 }
