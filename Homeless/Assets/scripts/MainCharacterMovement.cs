add18094aa1935e120219eb4a8f984c0263dd8f7
Dominic Potzinger <dominic.potzinger@student.tugraz.at>
2018-01-09 01:58:02 +0100

    Implement Keyboard Movement support

--- Homeless/Assets/scripts/MainCharacterMovement.cs
+++ Homeless/Assets/scripts/MainCharacterMovement.cs
@@ -4,27 +4,53 @@ using UnityEngine;
 
 public class MainCharacterMovement : PausableObject {
 
-  private enum MouseButton{LEFT, RIGHT, MIDDLE};
+  private enum MouseButton { LEFT, RIGHT, MIDDLE };
   private Vector3 targetPosition;
   public float movementSpeed;
-	
+
   void Start() {
     targetPosition = this.transform.position;
   }
 
-	protected override void updatePausable () {
+  protected override void updatePausable() {
+    float step = movementSpeed * Time.deltaTime;
+    handleMouseMovementInput();
+    handleKeyboardMovementInput(step);
 
+    updatePosition(step);
+  }
+
+  private void handleMouseMovementInput() {
     if (Input.GetMouseButtonDown((int)MouseButton.LEFT)) {
       Debug.Log("LMB");
       targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
       targetPosition.z = this.transform.position.z;
       Debug.Log("Mouse clicked position in world is: " + targetPosition);
     }
-    float step = movementSpeed * Time.deltaTime;
+  }
+
+  private void handleKeyboardMovementInput(float step) {
+    if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")) {
+      targetPosition = this.transform.position;
+    }
+
+    if (Input.GetKey("w")) {
+      targetPosition += new Vector3(0, step);
+    }
+    if (Input.GetKey("a")) {
+      targetPosition += new Vector3(-step, 0);
+    }
+    if (Input.GetKey("s")) {
+      targetPosition += new Vector3(0, -step);
+    }
+    if (Input.GetKey("d")) {
+      targetPosition += new Vector3(step, 0);
+    }
+  }
+
+  private void updatePosition(float step) {
     if (this.transform.position != targetPosition && !Physics2D.Raycast(this.transform.position, (targetPosition - this.transform.position).normalized, step)) {
       this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, step);
     }
-
   }
-
 }
