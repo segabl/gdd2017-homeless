95e10be9e6a98848db6a5a63abac54884a5de559
mtoller <maximilian.toller@student.tugraz.at>
2018-02-06 16:34:55 +0100

    Refactored Inventory

--- Homeless/Assets/scripts/Inventory.cs
+++ Homeless/Assets/scripts/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,13 +9,13 @@ public class Inventory : MonoBehaviour{
 
   private int nrOfSlots;
   //TODO: possibly replace value of items with Collectible
-  private Dictionary<string, int> items;
+  private Dictionary<Collectible, int> items;
   Button[] buttons;
 
   void Start() {
     nrOfSlots = GameController.instance.panelInventory.GetComponentsInChildren<Button>().Length;
     buttons = GameController.instance.panelInventory.GetComponentsInChildren<Button>();
-    items = new Dictionary<string, int>();
+    items = new Dictionary<Collectible, int>();
   }
 
   public void addItem(Collectible item) {
@@ -23,55 +24,73 @@ public class Inventory : MonoBehaviour{
       Debug.Log("Inventory is full");
       return;
     }
-    
-    if(items.ContainsKey(item.name)) {
-      items[item.name] += 1;
+
+    var match = findMatch(item);
+    if (match != null) {
+      items[match] += 1;
       Debug.Log("Added instance of item " + item.name);
       foreach(Button button in buttons) {
         if(button.GetComponentInChildren<Text>().text == item.name) {
-          button.GetComponentsInChildren<Text>()[1].text = items[item.name].ToString();
+          button.GetComponentsInChildren<Text>()[1].text = items[match].ToString();
           Debug.Log("Added item instances to text of " + button.name);
           break;
         }
       }
     }
     else {
-      items[item.name] = 1;
+      items[item] = 1;
       Debug.Log("Created new item entry " + item.name + " in inventory");
       Button button = buttons[items.Count - 1];
       buttons = GameController.instance.panelInventory.GetComponentsInChildren<Button>();
       Text itemName = button.GetComponentInChildren<Text>();
       itemName.text = item.name;
       Debug.Log("Added item name to text of " + button.name);
-      itemName.GetComponentsInChildren<Text>()[1].text = items[item.name].ToString();
+      itemName.GetComponentsInChildren<Text>()[1].text = items[item].ToString();
       Debug.Log("Added item instances to text of " + button.name);
     }    
     showInventoryInfoDebug();
   }
 
   private void showInventoryInfoDebug() {
-    foreach(KeyValuePair<string, int> item in items) {
-      Debug.Log("Item: " + item.Key + " Instances: " + item.Value);
+    foreach(KeyValuePair<Collectible, int> item in items) {
+      Debug.Log("Item: " + item.Key.name + " Instances: " + item.Value);
     }
   }
 
   public void removeSingleInstanceOfItem(Collectible item) {
-    items[item.name] -= 1;
-    removeIfNoInstances(item.name);
+    var match = findMatch(item);
+    if (match == null)
+    {
+      Debug.Log("Failed to remove Item: Invalid Key");
+      return;
+    }
+    items[match] -= 1;
+    removeIfNoInstances(match);
     Debug.Log("1 instance of " + item.name + " removed from inventory");
   }
 
   public void removeMultipleInstancesOfItem(Collectible item, int count) {
-    items[item.name] -= count;
-    removeIfNoInstances(item.name);
+    var match = findMatch(item);
+    if (match == null)
+    {
+      Debug.Log("Failed to remove Item: Invalid Key");
+      return;
+    }
+    items[match] -= count;
+    removeIfNoInstances(match);
     Debug.Log(count + " instances of " + item.name + " removed from inventory");
   }
 
-  private void removeIfNoInstances(string name) {
-    if (items[name] <= 0) {
-      items.Remove(name);
+  private void removeIfNoInstances(Collectible type) {
+    if (items[type] <= 0) {
+      items.Remove(type);
       Debug.Log(name + " deleted from inventory");
     }
+  }
+  private Collectible findMatch(Collectible item)
+  {
+    Collectible match = items.Keys.FirstOrDefault(type => (type.name == item.name));
+    return match;
   }
 
 }
