aea897977b7c8fee1cfaed26ab631e30a61ff4fd
mtoller <maximilian.toller@student.tugraz.at>
2018-02-06 14:46:02 +0100

    Repaired character interaction

--- Homeless/Assets/scripts/CharacterInteraction.cs
+++ Homeless/Assets/scripts/CharacterInteraction.cs
@@ -8,7 +8,7 @@ public class CharacterInteraction : InteractionHandler {
   public override void interact() {
     text.text = "This is a character text";
     Debug.Log("Character Interaction");
-    endInteraction();
+    //endInteraction();
   }
 
 }
