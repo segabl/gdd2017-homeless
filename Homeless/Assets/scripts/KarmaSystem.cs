b2545159a19d12d49338b60aa750938e7d4341e1
mtoller <maximilian.toller@student.tugraz.at>
2018-02-08 12:35:38 +0100

    Karma System: First steps

---
+++ Homeless/Assets/scripts/KarmaSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KarmaSystem
+{
+  public class KarmaController : PausableObject
+  {
+    private List<Karma> karmas;
+
+    protected override void updatePausable()
+    {
+      
+    }
+
+    void awake()
+    {
+      GameObject[] characters = GameObject.FindGameObjectsWithTag("character");
+      foreach (GameObject character in characters)
+      {
+        karmas.Add(new Karma());
+      }
+    }
+
+  }
+  internal class Karma
+  {
+    public GameObject character;
+    public List<Relationship> relationships;
+    public Reputation reputation;
+  }
+  internal class Relationship
+  {
+    public GameObject character;
+    public int trust { get; set; }
+    public int affection { get; set; }
+
+  }
+  class Reputation
+  {
+    public int charity { get; set; }
+    public int reliability { get; set; }
+    public int criminality { get; set; }
+    public int cruelty { get; set; }
+  }
+}
