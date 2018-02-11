9cd45390d3be928752651c2dd42c2f8f454e4b6f
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-02-11 17:01:46 +0100

    Add auto sizing of tileable collider objects
    Removed unnecessary fence prefab objects and adjusted colliders

---
+++ Homeless/Assets/scripts/AutoAdjustCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AutoAdjustCollider : MonoBehaviour {
+
+  public float widthAdjustment;
+  public float heightAdjustment;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector2 spriteSize = GetComponent<SpriteRenderer>().size;
+    GetComponent<BoxCollider2D>().size = new Vector2(spriteSize.y + widthAdjustment, spriteSize.y + heightAdjustment);
+	}
+}
