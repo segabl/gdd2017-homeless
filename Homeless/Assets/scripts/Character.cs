592a8060718cc3312d446b6a93ad3f526213060f
Christian Huber <chuber@student.tugraz.at>
2018-02-15 23:29:22 +0100

    Update player stats
    + Set daylength to 24 minutes

--- Homeless/Assets/scripts/Character.cs
+++ Homeless/Assets/scripts/Character.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character : PausableObject {
 
@@ -12,35 +13,73 @@ public class Character : PausableObject {
   public float repletion;
   public float health;
   public float sanity;
-  public float intoxication { get; set; } 
+  public float intoxication { get; set; }
+  private float accumulatedDeltaRepletion;
+  private float accumulatedDeltaHealth;
+  private float accumulatedDeltaSanity;
+  private float accumulatedDeltaIntox;
+  private bool alive;
 
   void Start () {
+    alive = true;
+    accumulatedDeltaRepletion = 0.0f;
+    accumulatedDeltaHealth = 0.0f;
+    accumulatedDeltaSanity = 0.0f;
+    accumulatedDeltaIntox = 0.0f;
     intoxication = 0.0f;
 	}
 
   protected override void updatePausable() {
-    updateRepletion();
-    updateHealth();
-    updateSanity();
-    updateIntoxication();
+    if (alive) {
+      //separate accumulated deltas for now, idk I'm a bit tired
+      accumulatedDeltaRepletion += Time.deltaTime;
+      accumulatedDeltaHealth += Time.deltaTime;
+      accumulatedDeltaSanity += Time.deltaTime;
+      accumulatedDeltaIntox += Time.deltaTime;
+      updateRepletion();
+      updateHealth();
+      updateSanity();
+      updateIntoxication();
+    }
+    #if UNITY_EDITOR
+      updateStatsDebugText();
+    #endif
+  }
+
+  private void updateStatsDebugText() {
+    if (this.name.Equals("MainCharacter")) {
+      Text[] texts = GameController.instance.menuCanvas.GetComponentsInChildren<Text>();
+      foreach (Text text in texts) {
+        if (text.name.Equals("StatsDebugText")) {
+          text.text = "Repletion is " + this.repletion + ", Health is " + this.health + ", Sanity is " + this.sanity + ", Intoxication level is " + this.intoxication;
+        }
+      }
+    }
   }
 
   public void adjustStats(float repletionAdjustment, float healthAdjustment, float sanityAdjustment, float intoxicationAdjustment) {
-    adjustRepletion(repletionAdjustment);
-    adjustHealth(healthAdjustment);
-    adjustSanity(sanityAdjustment);
-    adjustIntoxication(intoxicationAdjustment);
+    if (alive) {
+      adjustRepletion(repletionAdjustment);
+      adjustHealth(healthAdjustment);
+      adjustSanity(sanityAdjustment);
+      adjustIntoxication(intoxicationAdjustment);
+    }
   }
 
-  //TODO: stats < 0 -> die
   private void adjustRepletion(float repletionAdjustment) {
     this.repletion += repletionAdjustment;
     this.repletion = this.repletion > this.maxRepletion ? this.maxRepletion : this.repletion;
+    if(this.repletion <= 0.0f) {
+      die("Starvation");
+    }
   }
 
   private void adjustHealth(float healthAdjustment) {
     this.health += healthAdjustment;
     this.health = this.health > this.maxHealth ? this.maxHealth : this.health;
+    if (this.health <= 0.0f) {
+      die("Bad health condition");
+    }
   }
 
   private void adjustSanity(float sanityAdjustment) {
@@ -48,24 +87,54 @@ public class Character : PausableObject {
     this.sanity = this.sanity > this.maxSanity ? this.maxSanity : this.sanity;
   }
 
-  //TODO: intox to high -> die
   private void adjustIntoxication(float intoxicationAdjustment) {
     this.intoxication += intoxicationAdjustment;
+    this.intoxication = this.intoxication < 0.0f ? 0.0f : this.intoxication;
+    if(this.intoxication >= this.maxIntoxication) {
+      die("Too high intoxication level");
+    }
   }
 
   private void updateRepletion() {
-    //TODO some great update functionality
+    float minRef = accumulatedDeltaRepletion * 24.0f * 60.0f / GameController.instance.dayLength;
+    if (minRef >= 1.0f) {
+      adjustRepletion(-0.1f);
+      accumulatedDeltaRepletion = 0.0f;
+    }
   }
 
   private void updateHealth() {
-    //TODO some great update functionality
+    float healthDecrement = 0.0f;
+    float minRef = accumulatedDeltaHealth * 24.0f * 60.0f / GameController.instance.dayLength;
+    if (this.repletion <= 50.0f && minRef >= 1.0f) {
+      healthDecrement -= (this.maxRepletion - this.repletion) / this.maxRepletion + (this.maxSanity - this.sanity) / this.maxSanity + this.intoxication / this.maxIntoxication;
+      accumulatedDeltaHealth = 0.0f;
+    }
+    adjustHealth(healthDecrement);
   }
 
   private void updateSanity() {
-    //TODO some great update functionality
+    float sanityDecrement = 0.0f;
+    float minRef = accumulatedDeltaSanity * 24.0f * 60.0f / GameController.instance.dayLength;
+    if ((this.health <= 20.0f || this.repletion <= 50.0f) && minRef >= 1.0f) {
+      sanityDecrement -= (this.maxHealth - this.health) / this.maxHealth + (this.maxRepletion - this.repletion) / this.maxRepletion - this.intoxication / this.maxIntoxication;
+      accumulatedDeltaSanity = 0.0f;
+    }
+    adjustSanity(sanityDecrement);
   }
 
   private void updateIntoxication() {
-    //TODO some great update functionality
+    float hourRef = accumulatedDeltaIntox * 24 / GameController.instance.dayLength;
+    if (hourRef >= 1.0f) {
+      accumulatedDeltaIntox = 0.0f;
+      float intoxicationDecrement = -0.1f;
+      adjustIntoxication(intoxicationDecrement);
+    }
+  }
+
+  private void die(String reason) {
+    //TODO: switch to game over scene
+    this.alive = false;
+    Debug.Log("Player died because of: " + reason);
   }
 }
