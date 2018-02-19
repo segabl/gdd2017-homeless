d614baa7137b46af14f027c1212ffa5e2ddda9b3
mtoller <maximilian.toller@student.tugraz.at>
2018-02-19 15:13:17 +0100

    Low health and sanity effects

--- Homeless/Assets/scripts/PostProcessing.cs
+++ Homeless/Assets/scripts/PostProcessing.cs
@@ -5,16 +5,86 @@ using UnityEngine;
 public class PostProcessing : MonoBehaviour {
 
   public Shader daylightCycleShader;
+  public Shader blurShader;
+  public Shader redRadiationShader;
+  //public Shader swirlShader;
 
   private Material daylightCycleMaterial;
+  private Material blurMaterial;
+  private Material redRadiationMaterial;
+  //private Material swirlMaterial;
 
   void Awake() {
     daylightCycleMaterial = new Material(daylightCycleShader);
+    blurMaterial = new Material(blurShader);
+    redRadiationMaterial = new Material(redRadiationShader);
+    //swirlMaterial = new Material(swirlShader);
   }
 
   // Postprocess the image
   void OnRenderImage(RenderTexture source, RenderTexture destination) {
+
+
+    //GL.Clear(false, true, Color.black);
+
+
+    
+
+    ProcessDaylight(source, source);
+
+    ProcessPlayerHealth(source, source);
+    ProcessPlayerSanity(source, source);
+    //ProcessPlayerIntoxication(source, source);
+
+    Graphics.Blit(source, destination);
+
+    
+
+
+  }
+
+  internal void ProcessDaylight(RenderTexture source, RenderTexture destination)
+  {
     daylightCycleMaterial.SetFloat("_time", GameController.instance.dayTime);
     Graphics.Blit(source, destination, daylightCycleMaterial);
   }
+  internal void ProcessPlayerHealth(RenderTexture source, RenderTexture destination)
+  {
+    float health = GameController.instance.player.GetComponent<Character>().health;
+    if (health <= 5f)
+      health = 5f;
+    if (health < 80.0f)
+    {
+      redRadiationMaterial.SetFloat("_delta", (80 - health) / 160f);
+      if (health < 50.0f)
+      {
+        blurMaterial.SetFloat("hstep", 1.3f / health);
+        blurMaterial.SetFloat("vstep", 1.0f / health);
+        Graphics.Blit(source, source, blurMaterial);
+      }
+      
+      Graphics.Blit(source, destination, redRadiationMaterial);
+    }
+  }
+  internal void ProcessPlayerSanity(RenderTexture source, RenderTexture destination)
+  {
+    float sanity = GameController.instance.player.GetComponent<Character>().sanity;
+    if (sanity <= 3f)
+      sanity = 3f;
+    if (sanity < 80.0f)
+    {
+      blurMaterial.SetFloat("hstep", 3.20f / sanity);
+      blurMaterial.SetFloat("vstep", 1.80f / sanity);
+      Graphics.Blit(source, destination, blurMaterial);
+    }
+  }
+  /*internal void ProcessPlayerIntoxication(RenderTexture source, RenderTexture destination)
+  {
+    float intoxication = GameController.instance.player.GetComponent<Character>().intoxication;
+    if (intoxication > 0.0f)
+    {
+      
+    }
+  }*/
+
 }
---
+++ Homeless/Assets/shaders/RedRadiation.shader
@@ -0,0 +1,54 @@
+﻿Shader "Hidden/RedRadiation"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_delta ("Color change", Range (0, 1)) = 0
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			uniform float _delta;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				
+				return fixed4(max(col.r,min(col.r + _delta, 0.1)), min(col.g,max(col.g - _delta, 0.05)), min(col.b,max(col.b - _delta,0.05)), col.a);
+			}
+			ENDCG
+		}
+	}
+}
