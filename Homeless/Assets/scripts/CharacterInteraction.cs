a1affce0b09d875c15f7aa89c7ac032be42a29c6
Dominic Potzinger <dominic.potzinger@student.tugraz.at>
2018-02-20 01:36:01 +0100

    Implement Base for new Dialogue system

--- Homeless/Assets/scripts/CharacterInteraction.cs
+++ Homeless/Assets/scripts/CharacterInteraction.cs
@@ -3,28 +3,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using KarmaSystem;
+using UnityEngine.UI;
+using System.Reflection;
 
 public class CharacterInteraction : InteractionHandler {
 
+  [SerializeField]
+  Dialogues npc;
+  //[SerializeField]
+  //Text dialogueText;
+
+  int interactions = 0;
+
   public override void interact() {
     Debug.Log("Character Interaction");
-
-    //sample social interaction
-    Inventory player_inventory = GameController.instance.player.GetComponent<Inventory>();
-    Inventory this_inventory = gameObject.GetComponent<Inventory>();
-    if (player_inventory.giveItem("Alcohol", this_inventory))
-    {
-      GameController.instance.karmaController.SocialAction(GameController.instance.player, SocialConstants.sharingBeer,gameObject);
-      Debug.Log("Shared some beer");
-      text.text = "Thanks!";
-    }
-    else
-    {
-      text.text = "I'm soo thirsty...";
+    if (interactions == 0) {
+      npc.SetTree("default");
+    } else {
+      if (npc.GetChoices().Length > 0) {
+        npc.NextChoice(npc.GetChoices()[0]);
+      } else {
+        npc.Next();
+      }
     }
 
-    GameController.instance.karmaController.DebugKarmaList();
-    //endInteraction();
+    Display();
+    interactions++;
   }
 
+  public void Choice(int index) {
+    npc.NextChoice(npc.GetChoices()[index]); 
+    Display();
+  }
+
+  private void handleTrigger() {
+    if (npc.HasTrigger()) {
+      int res = callDecisionFunction(npc.GetTrigger());
+      if (npc.GetChoices().Length > 0) {
+        npc.NextChoice(npc.GetChoices()[res]);
+        handleTrigger();
+        return;
+      }
+    }
+  }
+
+  public void Display() {
+    handleTrigger();
+    text.text = npc.GetCurrentDialogue();
+  //  if (nextEnd == true) {
+  //    backPanel.SetActive(false);
+  //    nextTreeButton.SetActive(true);
+  //  } else {
+  //    backPanel.SetActive(true);
+  //    nextTreeButton.SetActive(false);
+  //  }
+
+  //  //Sets our text to the current text
+  //  dialogueText.text = npc.GetCurrentDialogue();
+  //  //Just debug log our triggers for example purposes
+  //  if (npc.HasTrigger())
+  //    Debug.Log("Triggered: " + npc.GetTrigger());
+  //  //This checks if there are any choices to be made
+  //  if (npc.GetChoices().Length != 0) {
+  //    //Setting the text's of the buttons to the choices text, in my case I know I'll always have a max of three choices for this example.
+  //    leftText.text = npc.GetChoices()[0];
+  //    middleText.text = npc.GetChoices()[1];
+  //    //If we only have two choices, adjust accordingly
+  //    if (npc.GetChoices().Length > 2)
+  //      rightText.text = npc.GetChoices()[2];
+  //    else
+  //      rightText.text = npc.GetChoices()[1];
+  //    //Setting the appropriate buttons visability
+  //    leftText.transform.parent.gameObject.SetActive(true);
+  //    rightText.transform.parent.gameObject.SetActive(true);
+  //    if (npc.GetChoices().Length > 2)
+  //      middleText.transform.parent.gameObject.SetActive(true);
+  //    else
+  //      middleText.transform.parent.gameObject.SetActive(false);
+  //  } else {
+  //    middleText.text = "Continue";
+  //    //Setting the appropriate buttons visability
+  //    leftText.transform.parent.gameObject.SetActive(false);
+  //    rightText.transform.parent.gameObject.SetActive(false);
+  //    middleText.transform.parent.gameObject.SetActive(true);
+  //  }
+
+  //  if (npc.End()) //If this is the last dialogue, set it so the next time we hit "Continue" it will hide the panel
+  //    nextEnd = true;
+  }
+
+  private int callDecisionFunction(String functionName) {
+    Type thisType = this.GetType();
+    MethodInfo method = thisType.GetMethod(functionName);
+    int result = (int) method.Invoke(this, new object[0]);
+    return result;
+  }
 }
