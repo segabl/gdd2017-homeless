119257e4297372de7829961752d7a7b31820012d
Dominic Potzinger <dominic.potzinger@student.tugraz.at>
2018-02-21 23:17:48 +0100

    Expand Dialogue System Triggers and tree traversal

--- Homeless/Assets/scripts/BeerGuyCharacterInteraction.cs
+++ Homeless/Assets/scripts/BeerGuyCharacterInteraction.cs
@@ -7,16 +7,7 @@ using UnityEngine;
 
 namespace Assets.scripts {
   class BeerGuyCharacterInteraction : CharacterInteraction {
-    public int hasBeer() {
-      Inventory player_inventory = GameController.instance.player.GetComponent<Inventory>();
-      if (player_inventory.containsItem("Alcohol")) {
-        return 0;
-      }
-      return 1;
-    }
-
-    public int giveBeerY() {
-      //sample social interaction
+    public void giveBeerY() {
       Inventory player_inventory = GameController.instance.player.GetComponent<Inventory>();
       Inventory this_inventory = gameObject.GetComponent<Inventory>();
       if (player_inventory.giveItem("Alcohol", this_inventory)) {
@@ -25,11 +16,11 @@ namespace Assets.scripts {
       }
 
       GameController.instance.karmaController.DebugKarmaList();
-      return 0;
+      SetNextTree("HappyBeerGuy");
     }
 
-    public int giveBeerN() {
-      return 0;
+    public void giveBeerN() {
+      
     }
   }
 }
