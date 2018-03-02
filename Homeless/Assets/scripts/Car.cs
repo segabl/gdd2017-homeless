94d988e92680d03f6de9fcd63fbfe58cd7a1859e
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-03-02 22:48:45 +0100

    Add car prefab

---
+++ Homeless/Assets/scripts/Car.cs
@@ -0,0 +1,26 @@
+﻿using SpriterDotNetUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Car : PausableObject {
+
+  public Color color = Color.white;
+
+  private UnityAnimator spriterAnimator;
+
+  void Start() {
+    spriterAnimator = gameObject.GetComponentInChildren<SpriterDotNetBehaviour>().Animator;
+    spriterAnimator.Speed = 0.0f;
+    foreach (SpriteRenderer renderer in gameObject.GetComponentsInChildren<SpriteRenderer>()) {
+      if (renderer.sprite.name == "color") {
+        renderer.color = color;
+      }
+    }
+  }
+
+  protected override void updatePausable() {
+
+  }
+}
