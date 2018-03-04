0ed6c0318b08f395fa1f9eadd47a62be7cb2c102
Anastasios Vulgaridis <a.voulgaridis@student.tugraz.at>
2018-03-04 16:12:18 +0100

    Thrashcans Timer And List Of Objects

--- Homeless/Assets/scripts/Thrascans.cs
+++ Homeless/Assets/scripts/Thrascans.cs
@@ -6,33 +6,42 @@ using UnityEngine.UI;
 
 public class Thrascans : InteractionHandler {
 
-	public GameObject item1;
-	public GameObject item2;
-	public GameObject item3;
+	public List<GameObject> items;
+	public float resetTime;
 
-		public override void interact() {
-			Debug.Log("Item interaction");
-		int num = Random.Range (1, 4);
-		Vector3 positionx = new Vector3 (1, 0, 0);
-		switch (num) {
-		case 1:
-			Instantiate (item1, transform.position+positionx, Quaternion.identity);
-			break;
 
-		case 2:
-			Instantiate (item2, transform.position+positionx,Quaternion.identity);
-			break;
+	void Start()
+	{
+		
+	}
+	public override void interact() {
+	Debug.Log ("Item interaction");
+		if (resetTime <= 0) {
+			int num = Random.Range (0, items.Count);
+			Vector3 positionx = new Vector3 (1, 0, 0);
 
-		case 3:
-			Instantiate (item3, transform.position + positionx, Quaternion.identity);
-			break;
-
-		}
-			/*Collectible c = new Collectible(this.name);
-			Inventory inventory = GameController.instance.player.GetComponent<Inventory>();
-			inventory.addItem(c);
-			this.gameObject.SetActive(false);*/
-			text.enabled = false;
+			Instantiate (items [num], transform.position + positionx, Quaternion.identity);
+			resetTime = 10;
+		} else {
+			
 		}
 	}
+	public bool isEmpty()
+	{
+		if(resetTime > 0)
+		{
+			return true;
+		}
+		return false;
+	}
+	void Update()
+	{
+		updatePausable ();
+		Debug.Log (resetTime);
+		if(resetTime > 0)
+		{
+			resetTime -= Time.deltaTime;
+		}
+	}
+}
 
