62b688494ca3f457470f161abdc7ea24bd60a284
Anastasios Vulgaridis <a.voulgaridis@student.tugraz.at>
2018-03-04 14:05:54 +0100

    WalkArea For Civilians

---
+++ Homeless/Assets/scripts/Police.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Police : MonoBehaviour {
+
+	public Transform target;
+	public float moveSpeed = 3;
+
+	void Start () {
+		
+		target = GameObject.Find("MainObject").transform;
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+
+		
+
+			Chase();
+
+	}
+
+	void Chase () {
+		Vector3 targetDirection = target.position - transform.position;
+		transform.position += targetDirection * moveSpeed * Time.deltaTime;
+	}
+}
