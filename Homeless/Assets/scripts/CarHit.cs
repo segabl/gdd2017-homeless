556efd5586c7ccdf9a7e74009d96029e269891db
Christian Huber <chuber@student.tugraz.at>
2018-03-05 20:50:11 +0100

    CarHit without physics

---
+++ Homeless/Assets/scripts/CarHit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarHit : MonoBehaviour {
+  void OnTriggerEnter2D(Collider2D col) {
+    Debug.Log("Collision with: " + col.gameObject.name);
+    col.gameObject.GetComponent<Car>().speed = 0;
+    GetComponent<CharacterAnimation>().playOnce("die", "NONE");
+  }
+}
